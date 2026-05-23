1516004f2eae83e49ac4b9d11517036e6491c47f
João Pedro Calaça Costa <jotao22gm@gmail.com>
2026-05-23 16:10:49 -0300

    TEMPO DE TOKEN TEMPORARIO PARA DESENVOLVIMENTO

--- config/settings.py
+++ config/settings.py
@@ -133,8 +133,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
 
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
