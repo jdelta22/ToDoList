b08f5d29ffed3da562c91262035fad6598e18973
João Pedro Calaça Costa <jotao22gm@gmail.com>
2026-05-24 12:42:48 -0300

    login e register

--- Frontend/TodoFront/src/pages/Login.jsx
+++ Frontend/TodoFront/src/pages/Login.jsx
@@ -1,9 +1,52 @@
+import { Link } from "react-router-dom"
+import "../styles/login.css"
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="auth-container">
+
+      <div className="auth-card">
+
+        <h1 className="auth-title">
+          Login
+        </h1>
+
+        <form className="auth-form">
+
+          <input
+            type="email"
+            placeholder="Email"
+            className="auth-input"
+          />
+
+          <input
+            type="password"
+            placeholder="Senha"
+            className="auth-input"
+          />
+
+          <button className="auth-button">
+            Entrar
+          </button>
+
+        </form>
+
+        <p className="auth-footer">
+          Não possui conta?{" "}
+
+          <Link
+            to="/register"
+            className="auth-link"
+          >
+            Registrar
+          </Link>
+
+        </p>
+
+      </div>
+
     </div>
   )
 }
 
-export default Login
+export default Login  
--- Frontend/TodoFront/src/pages/Register.jsx
+++ Frontend/TodoFront/src/pages/Register.jsx
@@ -1,7 +1,58 @@
+import { Link } from "react-router-dom"
+
+import "../styles/login.css"
+import "../styles/register.css"
+
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="auth-container">
+
+      <div className="auth-card">
+
+        <h1 className="auth-title">
+          Registro
+        </h1>
+
+        <form className="auth-form">
+
+          <input
+            type="text"
+            placeholder="Usuário"
+            className="auth-input"
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            className="auth-input"
+          />
+
+          <input
+            type="password"
+            placeholder="Senha"
+            className="auth-input"
+          />
+
+          <button className="register-button">
+            Criar conta
+          </button>
+
+        </form>
+
+        <p className="auth-footer">
+          Já possui conta?{" "}
+
+          <Link
+            to="/login"
+            className="auth-link"
+          >
+            Login
+          </Link>
+
+        </p>
+
+      </div>
+
     </div>
   )
 }
