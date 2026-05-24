49dfae0b1a83f0d9681217e34f965f2f02ee58ff
João Pedro Calaça Costa <jotao22gm@gmail.com>
2026-05-24 12:29:48 -0300

    Home

--- Frontend/TodoFront/src/pages/Home.jsx
+++ Frontend/TodoFront/src/pages/Home.jsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom"
+import "../styles/home.css"
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-container">
+
+      <div className="home-content">
+
+        <h1 className="home-title">
+          Organize suas tarefas com facilidade
+        </h1>
+
+        <p className="home-description">
+          Gerencie tarefas, compartilhe com amigos e acompanhe produtividade.
+        </p>
+
+        <div className="home-buttons">
+
+          <Link to="/register" className="primary-button">
+            Começar agora
+          </Link>
+
+          <Link to="/login" className="secondary-button">
+            Fazer login
+          </Link>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
